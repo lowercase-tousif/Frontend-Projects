759969d9637fb5231b6f319fc7c3c972b094eba3
lowercase-tousif <toisiftasrik@gmail.com>
2024-11-29 11:00:38 +0600

    Everytime page refresh we are rendering the task from the local storage

--- Todo/script.js
+++ Todo/script.js
@@ -1,31 +1,39 @@
-const inputField = document.getElementById('task-input');
-const addTaskBtn = document.getElementById('add-task');
-const taskContainer = document.getElementById('task-list');
+document.addEventListener('DOMContentLoaded', () => {
+    const inputField = document.getElementById('task-input');
+    const addTaskBtn = document.getElementById('add-task');
+    const taskContainer = document.getElementById('task-list');
 
-let tasks = [];
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-addTaskBtn.addEventListener('click', () => {
-    const task = inputField.value.trim();
-    if (task === "") return;
-
-    // generating random id
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    const randId = `${hours}: ${minutes}: ${seconds}`;
-    // creating object for the task
-    const taskObject = {
-        id: randId,
-        text: task,
-        completed: false,
+    const renderTasks = (task) => {
+        console.log(task);
     }
-    tasks.push(taskObject);
-    inputField.value = "";
-    saveTasks();
-    console.log(tasks);
-})
 
-const saveTasks = () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-}
+    // rendering all the tasks
+    tasks.forEach((task)=> renderTasks(task));
+
+    addTaskBtn.addEventListener('click', () => {
+        const task = inputField.value.trim();
+        if (task === "") return;
+
+        // generating random id
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const randId = `${hours}: ${minutes}: ${seconds}`;
+        // creating object for the task
+        const taskObject = {
+            id: randId,
+            text: task,
+            completed: false,
+        }
+        tasks.push(taskObject);
+        inputField.value = "";
+        saveTasks();
+    })
+
+    const saveTasks = () => {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+})
