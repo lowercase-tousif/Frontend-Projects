c6ea52b6b19fed93a593c5c02058ce512690cf6e
lowercase-tousif <toisiftasrik@gmail.com>
2024-11-29 10:51:57 +0600

    got time accepted upon task add

--- Todo/script.js
+++ Todo/script.js
@@ -4,13 +4,19 @@ const taskContainer = document.getElementById('task-list');
 
 let tasks = [];
 
-addTaskBtn.addEventListener('click',() => {
+addTaskBtn.addEventListener('click', () => {
     const task = inputField.value.trim();
-    if(task === "") return;
+    if (task === "") return;
 
+    // generating random id
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const randId = `${hours}: ${minutes}: ${seconds}`;
     // creating object for the task
     const taskObject = {
-        id: Date.now(),
+        id: randId,
         text: task,
         completed: false,
     }
